8ae559c153bc68a47b485f5b34c5f25c610fb7aa
Aditya Mishra <adityababaromio@gamil.com>
2025-10-01 18:28:30 +0530

    fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
   if (!token && request.nextUrl.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   return NextResponse.next();
