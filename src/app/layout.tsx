8b38385c0514bd9d6c1785a1996ad00fdd4cdfb5
Aditya Mishra <adityababaromio@gamil.com>
2025-10-04 00:44:58 +0530

    conflict resolve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import { Quicksand } from "next/font/google";
-<<<<<<< HEAD
 import LandingPage from "@/components/LandingPage";
-=======
->>>>>>> 8ae559c153bc68a47b485f5b34c5f25c610fb7aa
 
 // Load Quicksand font globally
 const quicksand = Quicksand({
@@ -24,14 +21,10 @@ export default function RootLayout({
     <html lang="en" className={`${quicksand.variable} font-sans`}>
       <body className="flex flex-col min-h-screen bg-white/95 text-gray-900">
         <Header />
-<<<<<<< HEAD
         <main className="flex-1 container mx-auto">{children}
           <LandingPage/>
         </main>
         
-=======
-        <main className="flex-1 container mx-auto p-4">{children}</main>
->>>>>>> 8ae559c153bc68a47b485f5b34c5f25c610fb7aa
         <Footer />
       </body>
     </html>
