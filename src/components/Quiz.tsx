c36b3d1b4f1a17ce84cf30739769ffd2a73c48af
Super Sheldon <tech@supersheldon.com>
2025-11-17 01:02:02 +0530

    Update Quiz.tsx

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -207,6 +207,8 @@ export default function Quiz({}: QuizProps) {
 
       setShowResult(true);
 
+      console.log("hello baby");
+
       setQuestionResults((prev) => ({
         ...prev,
         [currentQuestionIndex]: data.is_correct ? "correct" : "incorrect",
