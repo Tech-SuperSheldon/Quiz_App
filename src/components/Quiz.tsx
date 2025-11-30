f9508adffdf4576f0a857f5f18bd245f33d23b9d
shreyanshkushwaha23336 <shreyanshkushwaha23336@gmail.com>
2025-11-30 11:59:56 +0000

    quiz changes

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -13,7 +13,7 @@ const QUESTIONS_PER_STAGE = 10;
 const XP_PER_CORRECT = 5; 
 const BASE_BACKEND_URL = "https://92c52865-c657-478a-b2e0-625fc822f55b-00-23crg2t5cyi67.pike.replit.dev:5000";
 
-// --- INTERFACES ---
+// INTERFACES 
 interface Question {
   _id: string;
   question_name: string;
