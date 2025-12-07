e79e87490e47f7477d85df9a5109cf53e4672a0b
Super Sheldon <tech@supersheldon.com>
2025-12-07 21:36:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,13 @@ const nextConfig = {
     // your project has type errors.
     ignoreBuildErrors: true,
   },
+
+  env: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID:517061859674-s3agjbpk3lupq3b4fccqllejrp545i3s.apps.googleusercontent.com,
+    BASE_URL:https://levelupbackend.supersheldon.online/,
+  },
+
+  
   images: {
     remotePatterns: [
       {
